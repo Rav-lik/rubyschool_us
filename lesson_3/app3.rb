985abadacb304a1d1417577d97454c88b525c762
Andrey Moisseyev <ravl_kz@mail.ru>
2022-04-08 22:39:33 +0600

    Added apps from lessons 3 and 4

---
+++ lesson_3/app3.rb
@@ -0,0 +1,5 @@
+print 'Enter voltage: '
+volt = gets.to_i
+print 'Enter resistance: '
+resist = gets.to_i
+puts "Amperage: #{volt / resist}"
---
+++ lesson_4/app3.rb
@@ -0,0 +1,3 @@
+10.downto(-10) { |i| puts "i = #{i}, even: #{i.even?}" }
+#-10.upto(10) { |i| puts "i = #{i}, even: #{i.even?}" }
+#51.times { |i| puts "i = #{i}, even: #{i.even?}" }
